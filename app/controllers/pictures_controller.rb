e920e7502d356a376cbaf8a077961736b88e4f7c
Conrad Esche <conrad.3rd@gmail.com>
2022-10-23 16:56:28 +0200

    cleaner code

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,50 +3,13 @@ class PicturesController < ApplicationController
 
   # GET /pictures or /pictures.json
   def index
-
-    @pic_count = Picture.count
-    @pic_all = Picture.ids
-
-
     # @pictures = Picture.all
-    @pictures = Picture.where("id < ?", 10)
-
-    @atta_count = ActiveStorage::Attachment.count
-    @atta_show = ActiveStorage::Attachment.all
-
-
-    # User.joins(:posts, :account)
-
-    @sas = ActiveStorage::Attachment.select `&:picture`
-
-    @zwei = ActiveStorage::Attachment.joins(pictures: [:path])
-
+    # @pictures = Picture.where("id < ?", 10)
     @users = User.all
-
-    # @hashtag = ActiveRecord::Base.connection.execute("SELECT id, name FROM hashtags WHERE id IN (SELECT hashtag_id FROM pictures_hashtags WHERE picture_id = 1);").to_a
-
-    @pic_all.each do |id|
-      # @hashtag = ActiveRecord::Base.connection.execute("SELECT id, name FROM hashtags WHERE id IN (SELECT hashtag_id FROM pictures_hashtags WHERE picture_id = #{id});").to_a
-    end
   end
 
   def show
     @picture = Picture.find(params[:id])
-    # @user = User.find(params[:id])
-    # @user = User.find(params[:user_id])
     @user = Picture.where(id: @picture)
-    # @picture = User.find(params[:id])
-    # @upload = Upload.find(params[:id])
-    # @picturee = @picture.where(record_id: @picture)
-
   end
-
-
-
 end
-
-
-# active_employees = Employee.where(status: "active")
-# active_employees.select do |employee|
-#   employee.company.verified?
-# end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,7 @@
 class Picture < ApplicationRecord
-  #belongs_to :user
   has_one :user
+  belongs_to :user
   #has_many :pictures
 
-  # belongs_to :upload
-  # has_many_attached :pictures
 
-  # aa = ActiveStorage::Attachment
-  # belongs_to :aa
 end
