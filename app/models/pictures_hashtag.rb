66c287bb612ee905deb3a07a63686eb9c78e5696
Conrad Esche <conrad.3rd@gmail.com>
2022-11-02 18:00:04 +0100

    Searchfield + validation

--- app/models/pictures_hashtag.rb
+++ app/models/pictures_hashtag.rb
@@ -7,7 +7,7 @@ class PicturesHashtag < ApplicationRecord
   # belongs_to :user
 
   # validates :hashtag_id, :picture_id, uniqueness: true
-  validates :hashtag_id, uniqueness: { scope: :picture_id, notice: "Hashtag was successfully added." }#, acceptance: { message: 'must be abided' }
+  validates :hashtag_id, uniqueness: { scope: :picture_id }
 
 
 end
